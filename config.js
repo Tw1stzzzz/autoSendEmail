0d9508c86aaff47aa6f82c186872b2597aa47b29
Tw1stzzzz <110531243+Tw1stzzzz@users.noreply.github.com>
2022-08-22 12:24:17 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 // 配置信息
 module.exports = {
-  fromDisplayText: '阳哥', // 收件箱展示的来件人名字
+  fromDisplayText: '小绿', // 收件箱展示的来件人名字
   fromDisplaySubText: '每日提醒', // 收件箱展示的次级标题
-  user: 'xxx', // 发送者邮箱
-  pass: 'xxx', // 发送者邮箱MTP协议密码
-  to: 'xxx', // 发送到谁，填邮箱
-  weatherKey: '33369e365fe84eb68876f52a2ae51cca', // 和风天气key
-  location: 'xxx',  // 和风天气-成都武侯区的id
+  user: '2295497244@qq.com', // 发送者邮箱
+  pass: 'PHZ@520131466', // 发送者邮箱MTP协议密码
+  to: '2295497244@qq.com', // 发送到谁，填邮箱
+  weatherKey: '73f7f974d26349199b7515709cdb31db', // 和风天气key
+  location: '东莞',  // 和风天气-成都武侯区的id
   type: '1,3,9', // 和风天气-生活指数type
-  tianXingKey: 'eb75297818f2413f24e1f1f76662bccd', // 天行数据的key
-  startDay: '2015-09-29', // 在一起的日期
+  tianXingKey: '3907cbe824d9e8a667efe9a0b030441e', // 天行数据的key
+  startDay: '2022-03-27', // 在一起的日期
 }
